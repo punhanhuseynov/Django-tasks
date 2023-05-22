6b900a001971ef9dc2b000b6fc4030afc4310bb7
Punhan Huseynov <husepunhan@gmail.com>
2023-05-22 21:09:37 +0400

    translator added

---
+++ translator/app/static/js/app.js
@@ -0,0 +1,12 @@
+// Define Variables
+let langData1=document.getElementById('langdata-1')
+let langData2=document.getElementById('langdata-2')
+let word=document.getElementById('word')
+let translateBtn=document.getElementById('tbtn')
+
+translateBtn.onclick=()=>{
+    translateWord(word.value)
+}
+function translateWord(w){
+    
+}
