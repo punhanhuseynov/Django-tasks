1d3d6d3b9590cea9b3dd8248f01101d218285f94
Punhan Huseynov <husepunhan@gmail.com>
2023-06-04 15:16:55 +0400

    -

--- testdrf/app/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app.models import *
-
-class BookSerializers(serializers.ModelSerializer):
-    class Meta:
-        model=Books
-        fields="__all__"
--- testdrf/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Books(models.Model):
-    author=models.CharField(max_length=25)
-    title=models.CharField(max_length=40)
-    desc=models.TextField()
-    
